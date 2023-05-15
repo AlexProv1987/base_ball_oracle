2fa8c7c9d0a84f552f086698b88dc05bdb7ee8a5
AlexProv1987 <alex.provenzano87@gmail.com>
2023-05-15 14:53:51 -0500

    host

--- base_ball_oracle/settings.py
+++ base_ball_oracle/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['base-ball-oracle-dev.us-east-1.elasticbeanstalk.com',]
 
 
 # Application definition
