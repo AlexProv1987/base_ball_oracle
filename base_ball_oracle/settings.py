ad3b59e2c525307a99d78350215552e4d34c93f9
AlexProv1987 <alex.provenzano87@gmail.com>
2023-05-16 16:41:04 -0500

    allowed host

--- base_ball_oracle/settings.py
+++ base_ball_oracle/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['base-ball-oracle-dev.us-east-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['base-ball-oracle-dev.us-east-1.elasticbeanstalk.com', 'http://apitest.aprovtestdmns.com',]
 
 
 # Application definition
