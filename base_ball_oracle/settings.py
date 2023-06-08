2e93a3a3bb1c349ecc39eaeec37e9f9e2a3dbb6d
AlexProv1987 <alex.provenzano87@gmail.com>
2023-06-08 13:24:27 -0500

    white listing

--- base_ball_oracle/settings.py
+++ base_ball_oracle/settings.py
@@ -76,6 +76,7 @@ CORS_ORIGIN_WHITELIST = (
     "https://stable.d17lslnqu904pd.amplifyapp.com",
     "https://master.d1h7gu3k5d0iyx.amplifyapp.com",
     "https://www.baseballoracle.org",
+    "https://master.d203db3lu1c7s3.amplifyapp.com",
     )
 
 TEMPLATES = [
