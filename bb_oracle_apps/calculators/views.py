f8ce78caa5fec947ab3844627be45f6809bfb18d
AlexProv1987 <alex.provenzano87@gmail.com>
2023-07-06 09:12:15 -0500

    Catchers Mitt and Bat Drop

--- bb_oracle_apps/calculators/views.py
+++ bb_oracle_apps/calculators/views.py
@@ -16,9 +16,18 @@ class GloveView(APIView, GloveSize, ValidateParamsMixIn):
     accepted_params = {"age": int.__name__, "position": str.__name__}
 
     def get(self, request, *args, **kwargs):
-        self.set_player_level(int(request.query_params["age"]))
-        size = self.get_glove_size(request.query_params["position"])
-        scrape_product = WebScraper('https://www.google.com/search', 'https://www.google.com/' ,30,
+        if self.validate_keys(request, "all"):
+            self.set_player_level(int(request.query_params["age"]))
+            size = self.get_glove_size(request.query_params["position"])
+            search_string = self.create_scrape_string(size)
+            if search_string is None:
+                return Response(
+                data={
+                    "error": "Invalid Params",
+                    "available": self.get_accepted_params(),
+                },status=status.HTTP_400_BAD_REQUEST,)
+            else:
+                scrape_product = WebScraper('https://www.google.com/search', 'https://www.google.com/' ,30,
                                   node_dict={
                                   'product_url':'//div[@class="zLPF4b"]/span["@class=eaGTj mQaFGe shntl"]/div/a/@href',
                                   'product_name':'//div[@class="EI11Pd Hb793d"]/h3[@class="tAxDx"]/text()',
@@ -28,17 +37,32 @@ class GloveView(APIView, GloveSize, ValidateParamsMixIn):
                                   'product_img': '//div[@class="ArOc1c"]/img/@data-image-src',
                                   },
                                   params={
-                                   'q':f'{GEAR_SPONSOR} {size} inch baseball glove',
+                                   'q':search_string,
                                    'hl':"en",
                                    'gl':"us",
                                    'tbm':"shop",
                                     })
-        products = scrape_product.scrape_first_item()
-        return Response(
-            data={'size':size,'product':products},
-            status=status.HTTP_200_OK,
-        )
-
+                products = scrape_product.scrape_first_item()
+                return Response(
+                data={'size':size,'product':products},
+                status=status.HTTP_200_OK,
+                )
+        else:
+            return Response(
+                data={
+                    "error": "Invalid Params",
+                    "available": self.get_accepted_params(),
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+    
+    def create_scrape_string(self,size):
+        scrape_string = None
+        if self.request.query_params["position"] == 'catcher':
+            scrape_string = f'{GEAR_SPONSOR} {self.request.query_params["position"]} {size} mitt'
+        else:
+            scrape_string = f'{GEAR_SPONSOR} {size} baseball glove'
+        return scrape_string
 
 class BatView(APIView, BatSize, ValidateParamsMixIn):
     accepted_params = {"height": str.__name__, "weight": int.__name__}
@@ -53,6 +77,7 @@ class BatView(APIView, BatSize, ValidateParamsMixIn):
                     data={"bat_size": "None Found"}, status=status.HTTP_200_OK
                 )
             else:
+                drop = self.get_bat_drop(16)
                 scrape_product = WebScraper('https://www.google.com/search', 'https://www.google.com/' ,30,
                                   node_dict={
                                   'product_url':'//div[@class="zLPF4b"]/span["@class=eaGTj mQaFGe shntl"]/div/a/@href',
@@ -63,7 +88,7 @@ class BatView(APIView, BatSize, ValidateParamsMixIn):
                                   'product_img': '//div[@class="ArOc1c"]/img/@data-image-src',
                                   },
                                   params={
-                                   'q':f'{GEAR_SPONSOR} {bat} inch bat',
+                                   'q':f'{GEAR_SPONSOR} {bat} inch bat {drop} drop',
                                    'hl':"en",
                                    'gl':"us",
                                    'tbm':"shop",
